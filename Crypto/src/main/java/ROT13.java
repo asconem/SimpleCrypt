cd204c2406f53e8ebc87f65ceb4a955ddd42386c
asconem <asconem@gmail.com>
2020-03-16 09:22:42 -0400

    Tests pass

--- Crypto/src/main/java/ROT13.java
+++ Crypto/src/main/java/ROT13.java
@@ -1,57 +1,105 @@
 import static java.lang.Character.isLowerCase;
 import static java.lang.Character.isUpperCase;
 import static java.lang.Character.toLowerCase;
+
 public class ROT13  {
+    int difference;
 
-    ROT13(Character cs, Character cf) {
-        String alphabet = "abcdefghijklmnopqrstuvwxyz";
-        //alphabet.split("");
-        char[] list = alphabet.toCharArray();
-
-        int csNumber = 0;
-        for (int i = 0; i < list.length -1; i++) {
-            if (list[i] == cs) {
-                csNumber = i;
-            }
-        }
-
-        int cfNumber = 0;
-        for (int i = 0; i < list.length -1; i++) {
-            if (list[i] == cf) {
-                cfNumber = i;
-            }
-        }
-
-        int numOfRotations = cfNumber - csNumber;
-
+    public ROT13(Character cs, Character cf) {
+        difference = (cf - cs);
     }
 
-    ROT13() {
-
+    public ROT13() {
+        difference = 13;
     }
 
 
     public String crypt(String text) throws UnsupportedOperationException {
-
-        return "";
+        String rotate = "";
+        for (Character c : text.toCharArray()) {
+            if(Character.isAlphabetic(c)) {
+                int code = c - difference;
+                if (Character.isUpperCase(c)) {
+                    if (code < 'A') {
+                        code = (1 + 'Z') - ('A' - code);
+                    }
+                } else {
+                    if (code < 'a') {
+                        code = (1 + 'z') - ('a' - code);
+                    }
+                }
+                rotate += (char) code;
+            } else {
+                rotate += c;
+            }
+        }
+        System.out.println(rotate);
+        return rotate;
     }
 
+
     public String encrypt(String text) {
-        return text;
+        String rotate = "";
+        for (Character c : text.toCharArray()) {
+            if(Character.isAlphabetic(c)) {
+                int code = c - difference;
+                if (Character.isUpperCase(c)) {
+                    if (code < 'A') {
+                        code = (1 + 'Z') - ('A' - code);
+                    }
+                } else {
+                    if (code < 'a') {
+                        code = (1 + 'z') - ('a' - code);
+                    }
+                }
+                rotate += (char) code;
+            } else {
+                rotate += c;
+            }
+        }
+        System.out.println(rotate);
+        return rotate;
     }
 
     public String decrypt(String text) {
-        return text;
+        String rotate = "";
+        for (Character c : text.toCharArray()) {
+            if(Character.isAlphabetic(c)) {
+                int code = c + difference;
+                if (Character.isUpperCase(c)) {
+                    if (code > 'Z') {
+                        code = ('A' - 1) + (code - 'Z');
+                    }
+                } else {
+                    if (code > 'z') {
+                        code = ('a' - 1) + (code - 'z');
+                    }
+                }
+                rotate += (char) code;
+            } else {
+                rotate += c;
+            }
+        }
+        System.out.println(rotate);
+        return rotate;
     }
 
     public static String rotate(String s, Character c) {
-        //for (int i = 0; i < s.length(); i++) {
-//            c = s.charAt(i);
-//            if       (c >= 'a' && c <= 'm') c += 13;
-//            else if  (c >= 'A' && c <= 'M') c += 13;
-//            else if  (c >= 'n' && c <= 'z') c -= 13;
-//            else if  (c >= 'N' && c <= 'Z') c -= 13;
-        return "";
+        int index = 0;
+        for (; index < s.length(); index++) {
+            if(s.charAt(index) == c) {
+                break;
+            }
+        }
+
+        String rotate = "";
+        for (int j = 0; j < s.length(); j++, index++) {
+            if (index >= s.length()) {
+                index = 0;
+            }
+            rotate += s.charAt(index);
+        }
+        return rotate;
     }
 
 }
